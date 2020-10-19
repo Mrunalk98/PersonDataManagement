b4e7108a71753be9e62672ab9e1008fc72b11de5
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-19 10:39:55 +0530

    [mrunal] Add. Added records in personDetails list

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -9,6 +9,19 @@ namespace PersonDataManagement
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Person Data Management Problem !");
+            List<Person> personDetails = new List<Person>();
+            AddRecords(personDetails);
+        }
+
+        //UC 1
+        private static void AddRecords(List<Person> personDetails)
+        {
+            personDetails.Add(new Person("254589652", "John", "Mumbai", 15));
+            personDetails.Add(new Person("254589652", "Sam", "Nagpur", 50));
+            personDetails.Add(new Person("254589652", "Joey", "Kerela", 17));
+            personDetails.Add(new Person("254589652", "Alan", "Delhi", 44));
+            personDetails.Add(new Person("254589652", "Jim", "Jaipur", 20));
+            personDetails.Add(new Person("254589652", "Sim", "Mumbai", 80));
         }
     }
 }
