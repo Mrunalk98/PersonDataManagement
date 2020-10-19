a75ae1513429355ecfe8aa3a3bed2911039b0cad
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-19 10:36:37 +0530

    [mrunal] Add. Added person class and welcome message

---
+++ PersonDataManagement/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonDataManagement
+{
+    public class Person
+    {
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+        public Person(string ssn, string name, string address, int age)
+        {
+            this.SSN = ssn;
+            this.Name = name;
+            this.Address = address;
+            this.Age = age;
+        }
+    }
+}
---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Data Management Problem !");
+        }
+    }
+}
