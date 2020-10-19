f1a711b0acd1ada7ed0781f2e4242e95c97fd58d
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-19 10:43:02 +0530

    [mrunal] Add. Added a method to get top 2 records having age less than 60

--- PersonDataManagement/Program.cs
+++ PersonDataManagement/Program.cs
@@ -11,6 +11,7 @@ namespace PersonDataManagement
             Console.WriteLine("Welcome to Person Data Management Problem !");
             List<Person> personDetails = new List<Person>();
             AddRecords(personDetails);
+            RetrieveTop2Records_ForAgeLessThan60(personDetails);
         }
 
         //UC 1
@@ -22,6 +23,16 @@ namespace PersonDataManagement
             personDetails.Add(new Person("254589652", "Alan", "Delhi", 44));
             personDetails.Add(new Person("254589652", "Jim", "Jaipur", 20));
             personDetails.Add(new Person("254589652", "Sim", "Mumbai", 80));
+        }
+
+        // UC 2
+        private static void RetrieveTop2Records_ForAgeLessThan60(List<Person> personDetails)
+        {
+            Console.WriteLine("Top 2 persons having age less than 60 : ");
+            foreach (Person person in personDetails.FindAll(p => p.Age < 60).Take(2).ToList())
+            {
+                Console.WriteLine("Name : " + person.Name + "\t Age : " + person.Age);
+            }
         }
     }
 }
